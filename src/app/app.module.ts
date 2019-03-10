44e0b0373458297c0a42b96200a7e785ee72786a
Andres <andsc0830900@gmail.com>
2019-03-10 19:52:43 +0100

    ready!

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,37 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+// Angular Material
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module';
+import { MapaComponent } from './components/mapa/mapa.component';
+
+
+// GMPAPS
+import { AgmCoreModule } from '@agm/core';
+import { MapaEditarComponent } from './components/mapa/mapa-editar.component';
+
+// FORMS
+import { ReactiveFormsModule } from '@angular/forms';
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MapaComponent,
+    MapaEditarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'YOUR_KEY'
+    }),
+    ReactiveFormsModule
+  ],
+  entryComponents: [
+    MapaComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/classes/marcador.class.ts
@@ -0,0 +1,9 @@
+
+export class Marcador {
+
+  constructor(
+    public lat: number,
+    public lng: number,
+    public titulo: string = 'Sin Titulo',
+    public desc: string = 'Sin Descripción' ) { }
+}
